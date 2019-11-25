0e0d3ec3fbba9fe22ffe44b03659d83b5deaeee1
hannes <h.vietz@gmail.com>
2019-11-25 16:17:51 +0100

    removed nearly everything

--- init.el
+++ init.el
@@ -9,7 +9,6 @@
 	     '("melpa" . "http://melpa.org/packages/") t)
 
 (package-initialize)
-(add-hook 'after-init-hook 'global-company-mode)
 
 (when (not package-archive-contents)
   (package-refresh-contents)
@@ -19,9 +18,7 @@
   '(better-defaults
     material-theme
     magit
-    json-mode
-    eglot
-    company))
+    ))
 
 (mapc #'(lambda (package)
 	  (unless (package-installed-p package)
@@ -38,35 +35,3 @@
 (ido-mode 1)
 (electric-indent-mode 0)
 (setq x-select-enable-clipboard t)
-
-
-(defun format_on_save ()
-  ;; Format on save
-  (when (eq major-mode 'python-mode)
-    (eglot-format)
-)
-)
-
-(add-hook 'before-save-hook #'format_on_save)
-
-
-
-
-;; ;; PYTHON CONFIGURATION
-;; ;; --------------------------------------
-(add-hook 'python-mode-hook 'eglot-ensure)
-
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (company eglot))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
